01ccc37e22fa4c0976db044e64958db99f75396a
Patricio López Cerda <patricio.lopez@consensys.net>
2022-11-01 22:49:51 -0300

    Update 05_Herencia.sol

--- 05_Herencia.sol
+++ 05_Herencia.sol
@@ -23,3 +23,23 @@ contract Hijo is Padre {
         return(saludo,despedida);
     }
 }
+
+//Tambien podemos definir y heredar desde interfaces, que solo traen 
+//el signature de las funciones (ni implementacion ni estado)
+interface IAuthList {
+    function grant(address _grant) external;
+    function revoke(address _revoke) external;
+    function isAuthorized(address _toCheck) external view returns(bool); 
+}
+
+//o bien desde un contrato abstracto que puede tener parte de una implementacion
+ abstract contract Pago {
+
+    function iniciarPago() public {
+        //codigo
+    }
+
+    //Si no hay implementacion, la funcion debe marcarse como Virtual
+    function aprobar() virtual external;
+
+ }
